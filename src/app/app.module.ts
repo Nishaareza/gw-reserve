e1316cd86f81f4226e2542541d56337979e3a145
nishaareza <nishaareza@gmail.com>
2017-09-27 13:25:45 -0700

    rendering app loaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,18 @@
-/**
- * Created by nisha on 9/27/2017.
- */
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
